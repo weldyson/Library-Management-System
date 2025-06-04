47b1943f6c133b8eee40dde464079e76b24700a9
Azevedo <weldysona@gmail.com>
2025-06-03 21:33:54 -0300

    Library Management System

---
+++ src/Book.java
@@ -0,0 +1,56 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean available;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.available = true; // New book starts as available.
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", available=" + available +
+                '}';
+    }
+}
