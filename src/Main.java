1f46aa3d013e40fa67f4d0626a209aa0889806bd
Azevedo <weldysona@gmail.com>
2025-06-07 14:36:17 -0300

    main update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("\n=== Library Management System ===");
+            System.out.println("1. Add Book");
+            System.out.println("2. List Books");
+            System.out.println("3. Borrow Book");
+            System.out.println("4. Return Book");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+            option = scanner.nextInt();
+            scanner.nextLine(); // Clear buffer
+
+            switch (option) {
+                case 1:
+                    System.out.print("Enter the book title: ");
+                    String title = scanner.nextLine();
+                    System.out.print("Enter the book author: ");
+                    String author = scanner.nextLine();
+                    System.out.print("Enter the publication year: ");
+                    int year = scanner.nextInt();
+                    scanner.nextLine(); // Clear buffer
+                    Book book = new Book(title, author, year);
+                    library.addBook(book);
+                    break;
+
+                case 2:
+                    library.listBooks();
+                    break;
+
+                case 3:
+                    System.out.print("Enter the title of the book to borrow: ");
+                    String borrowTitle = scanner.nextLine();
+                    library.borrowBook(borrowTitle);
+                    break;
+
+                case 4:
+                    System.out.print("Enter the title of the book to return: ");
+                    String returnTitle = scanner.nextLine();
+                    library.returnBook(returnTitle);
+                    break;
+
+                case 5:
+                    System.out.println("Exiting the system...");
+                    break;
+
+                default:
+                    System.out.println("Invalid option! Try again.");
+            }
+        } while (option != 5);
+
+        scanner.close();
     }
 }
